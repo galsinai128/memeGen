4d0a81242957662c41634ecd3a897558d1bf0e6e
gal <galsinay128@gmail.com>
2018-06-20 17:43:15 +0300

    set text on img

--- js/canvas.js
+++ js/canvas.js
@@ -2,21 +2,27 @@
 
 var canvas;
 var ctx;
- 
+
 function initCanvas() {
     canvas = document.querySelector('.canvas');
     ctx = canvas.getContext('2d');
-      
+
 }
 
 function drawImg(imgUrl) {
     initCanvas();
-    
+
     canvas.width = 350;
     canvas.height = 350;
     var img = new Image()
     img.src = imgUrl.src;
     img.onload = function () {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        
     }
+}
+
+function drawText(elInput){
+    ctx.font = "30px Arial";
+    ctx.fillText(elInput, 10, 50);
 }
