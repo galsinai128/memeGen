03b3a3d72ac4eafe6d008e9914ed0a7446be7cc3
omers <omer3026@gmail.com>
2018-06-20 19:59:02 +0300

    canvas text-deltes and pos

--- js/canvas.js
+++ js/canvas.js
@@ -2,38 +2,90 @@
 
 var canvas;
 var ctx;
+var currImg = null;
+var gBottomTbY;
+var gBottomTbX;
+var gTbHeight = 50;
+var gPosTxt;
 
 function initCanvas() {
     canvas = document.querySelector('.canvas');
     ctx = canvas.getContext('2d');
+    canvas.width = 350;
+    canvas.height = 350;
+    gPosTxt = null;
+
+    gBottomTbY = (canvas.height - 50) - 10;
+    gBottomTbX = canvas.width - 20;
 
 }
 
 function drawImg(imgUrl) {
     initCanvas();
-    canvas.width = 350;
-    canvas.height = 350;
-    var img = new Image()
-    img.src = imgUrl.src;
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+    if (!currImg || currImg !== imgUrl) {
+        currImg = imgUrl;
+        var img = new Image()
+        img.src = imgUrl.src;
+        img.onload = function () {
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            drawRectTotxt();
+        }
+    } else {
+        ctx.drawImage(currImg, 0, 0, canvas.width, canvas.height);
         drawRectTotxt();
+    }
+}
+
+function drawText(ev, elInput) {
+    var inputValue = elInput.value;
+    ctx.font = "30px Impact";
+    ctx.textAlign = "left";
+    ctx.fillStyle = "white";
+    if (!gPosTxt) {
+        gPosTxt = 50;
+    }
+    if (ev.inputType === 'deleteContentBackward' && inputValue !== ' ') {
+        elInput.value = '';
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawImg(currImg);
+    } else {
+
+        ctx.fillText(inputValue, 15, gPosTxt, gBottomTbX);
+        ctx.strokeText(inputValue, 15, gPosTxt, gBottomTbX);
+
     }
 
 }
 
-function drawText(ev,elInput){
-    ctx.font = "30px Arial";
-    ctx.textAlign="left";
-    ctx.fillText(elInput, 15, 50);
-}
+function drawRectTotxt() {
 
-function drawRectTotxt(){
     ctx.fillStyle = "#afa8a8";
-    ctx.strokeRect(10,10, canvas.width - 20, 50);
-    ctx.strokeRect(10,(canvas.height-50)-10, canvas.width - 20, 50);
+    //first textbox
+    ctx.strokeRect(10, 10, gBottomTbX, gTbHeight);
 
+    //second textbox
+    ctx.strokeRect(10, gBottomTbY, gBottomTbX, gTbHeight);
 }
 
+function onCanvasClick(ev) {
+    var x;
+    var y;
+    if (ev.pageX || ev.pageY) {
+        x = ev.pageX;
+        y = ev.pageY;
+    }
+    else {
+        x = ev.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = ev.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+
+    if ((x > 10 && x < gBottomTbX) &&
+        (y > gTbHeight && y > gBottomTbY)) {
+        gPosTxt = gBottomTbY + 35;
+    }
+}
 
 
