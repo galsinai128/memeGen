9a6ea579ae7a11ce076d0fbfe73f1f0d701af27d
omers <omer3026@gmail.com>
2018-06-20 18:12:20 +0300

    text on canvas pos

--- js/canvas.js
+++ js/canvas.js
@@ -11,18 +11,29 @@ function initCanvas() {
 
 function drawImg(imgUrl) {
     initCanvas();
-
     canvas.width = 350;
     canvas.height = 350;
     var img = new Image()
     img.src = imgUrl.src;
     img.onload = function () {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        
+        drawRectTotxt();
     }
+
 }
 
-function drawText(elInput){
+function drawText(ev,elInput){
     ctx.font = "30px Arial";
-    ctx.fillText(elInput, 10, 50);
+    ctx.textAlign="left";
+    ctx.fillText(elInput, 15, 50);
 }
+
+function drawRectTotxt(){
+    ctx.fillStyle = "#afa8a8";
+    ctx.strokeRect(10,10, canvas.width - 20, 50);
+    ctx.strokeRect(10,(canvas.height-50)-10, canvas.width - 20, 50);
+
+}
+
+
+
